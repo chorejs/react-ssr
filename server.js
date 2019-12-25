b627c8a40ea2e37990681a6f22fda5b6a85754b6
fengd <fengdonglp@163.com>
2019-12-25 23:50:19 +0800

    fix: webpack编译后commonjs模块引入es6模块处理错误

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 const React = require('react');
-const ReactDom = require('react-dom');
 const { renderToString } = require('react-dom/server');
 const { StaticRouter } = require('react-router-dom');
 const App = require('./client/App');
 
+// webpack在编译模块时如果入口文件为commonjs，依赖模块为es6模块时，则导入的es6模块针对export default 则转换为 {defualt: 模块内容}
+
 app.get('*', (req, res) => {
   const content = renderToString(
-    (
-      <StaticRouter><App/></StaticRouter>
-    )
+    <StaticRouter>
+      <App.default />
+    </StaticRouter>
   );
   res.send(`
     <html>
@@ -22,11 +23,11 @@ app.get('*', (req, res) => {
         <script src="./client.js"></script>
       </body>
     </html>
-  `)
+  `);
 });
 
 app.use(express.static('./dist'));
 
 app.listen(3000, () => {
   console.log('react-ssr listening on port 3000!');
-})
+});
